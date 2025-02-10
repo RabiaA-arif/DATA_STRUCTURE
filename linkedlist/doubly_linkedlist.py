588647a3d280655b646642a50496d1a5d0622d6e
RabiaA-arif <rabiaarif694@gmail.com>
2025-02-10 21:24:26 +0500

    node adding

--- linkedlist/doubly_linkedlist.py
+++ linkedlist/doubly_linkedlist.py
@@ -20,13 +20,38 @@ class doubly_linkedlist:
             self.head.prev=new_node
             self.head=new_node
         
-    # def print(self):
-    #     temp=self.head
-    #     # self.temp=self.head
-    #     while temp:
-    #         print(temp.data, end="->")
-    #         temp=temp.next
-    #     print("None")
+    def insert_at_end(self,data):
+        new_node=Node(data)
+        if self.head is None:
+            self.head=new_node
+            return
+        end=self.head
+        while end.next:
+            end=end.next
+        end.next=new_node
+        new_node.prev=end
+    
+    def insert_before_node(self,given_node_data,data):
+        new_node=Node(data)
+        if self.head is None:
+            print("no node here for adding new node before that")
+            return
+        current=self.head
+        while current:
+            if current.data==given_node_data:
+                new_node.next=current
+                new_node.prev=current
+            
+            if current.prev:
+                current.prev=new_node
+            else:
+                self.head=new_node
+            current.prev=new_node
+            return
+        
+            current=current.next
+        print(f"node with value {given_node_data} not found")
+
         
     
     def print(self):
@@ -52,5 +77,17 @@ if __name__ == "__main__":
     print("node of linkedlist with value")
     doubly_linkedlist.print()
     
+    
+    print("adding node at the strat")
+    doubly_linkedlist.insert_at_end(1)
+    doubly_linkedlist.insert_at_end(2)
+    doubly_linkedlist.insert_at_end(3)
+    doubly_linkedlist.insert_at_end(4)
+    doubly_linkedlist.insert_at_end(5)
+    doubly_linkedlist.print()
+    
+    print("adding the node before given node data")
+    doubly_linkedlist.insert_before_node(4,44)
+    doubly_linkedlist.print()
 
 
